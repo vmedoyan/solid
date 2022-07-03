93ef2086c1a85d17509f67aba19f3070256e9d48
vmedoyan <vmedoyan@gmail.com>
2022-07-03 15:51:52 +0400

    Update Dependency-Inversion.kt

--- Dependency-Inversion.kt
+++ Dependency-Inversion.kt
@@ -1,1 +1,25 @@
+interface DBConnectionInterface
+{
+    fun connect();
+}
 
+class MySqlConnection:DBConnectionInterface {
+    override fun connect(){
+        
+    }
+}
+
+class PostgreSQLConnection:DBConnectionInterface {
+    override fun connect(){
+        
+    }
+}
+
+class Sql(_dbConnection:Any){
+    var dbConnection = _dbConnection
+}
+
+
+fun main() {
+   
+}
